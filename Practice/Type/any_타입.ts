8fb09a8541ee18ecc1f4ce2b15216a47b8eab44a
yujin-kwak <yujin02371@gmail.com>
2023-04-22 00:49:27 +0900

    any 타입

---
+++ Practice/Type/any_타입.ts
@@ -0,0 +1,11 @@
+// 명시적으로 any 타입 지정
+let product_id_any: any = 123468;
+
+// any로 설정되었으므로 어떤 타입의 값이라도 할당 가능
+product_id_any = 'q56231';
+
+// 암시적으로 any 타입 지정
+let product_id_any2;
+
+product_id_any2 = 123468;
+product_id_any2 = 'q56231';
